e17541f092e71cd13b68ceb12448e6d3a10e050a
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-05-30 13:00:02 +0300

    Add files via upload
    Fixed Password

---
+++ Fixed Password/Fixed Password/Fixed Password.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	while (true)
+	{
+		cin >> x;
+		if (x == 1999)
+		{
+			cout << "Correct" << endl;
+
+			return 0;
+		}
+		else {
+			cout << "Wrong" << endl;
+
+		}
+
+	}
+
+
+}
